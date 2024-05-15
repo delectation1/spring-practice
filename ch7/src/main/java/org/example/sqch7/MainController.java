f9dee0fb55539a834629c4dea45afe267597533a
gavwyh <wongyhpersonal@gmail.com>
2024-05-15 18:01:43 +0800

    finish ch7

---
+++ ch7/src/main/java/org/example/sqch7/MainController.java
@@ -0,0 +1,12 @@
+package org.example.sqch7;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+    @RequestMapping("/home")
+    public String home() {
+        return "home";
+    }
+}
